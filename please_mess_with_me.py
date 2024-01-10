482038c8bcca8060ed021558b41cb3561bf33919
Jiawei Li <lijiawei@msu.edu>
2024-01-09 21:14:23 -0500

    Modify codes

--- please_mess_with_me.py
+++ please_mess_with_me.py
@@ -6,3 +6,12 @@ c = True
 
 # print(a + b)
 print(b + c)
+
+# To correct the mistake, we might convert a to an integer before adding
+a = int(a)
+print(int(a) + b)
+
+# The following will also work:
+print (a+c)
+print (a*c)
+print(a-b)
